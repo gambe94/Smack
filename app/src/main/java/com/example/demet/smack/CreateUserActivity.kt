52943de2d285c4d37850b48b012b8e083c3a07bf
Demeter Gabor <gabor.demeter@attentioncrm.hu>
2018-01-24 12:18:33 +0100

    lesson-04

---
+++ app/src/main/java/com/example/demet/smack/CreateUserActivity.kt
@@ -0,0 +1,22 @@
+package com.example.demet.smack
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+    fun generateUserAvatar(view: View){
+
+    }
+    fun genarateColorClicked(view: View){
+
+    }
+    fun createUserBtnClicked(view: View){
+
+    }
+}
